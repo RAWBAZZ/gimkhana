a7c484de6478cbb6b28193bf5c7409119d2ce71e
RAWBAZZ <therealrawbazz@gmail.com>
2026-08-12 17:02:49 +0530

    Create SpotifyPlayer.tsx

---
+++ components/components/SpotifyPlayer.tsx
@@ -0,0 +1,15 @@
+export default function SpotifyPlayer() {
+return (
+<iframe
+style={{ borderRadius: 16 }}
+src="https://open.spotify.com/embed/playlist/75zwc1fWU4EKVU2jSzkSAq?utm_source=generator&theme=0"
+width="100%"
+height="380"
+frameBorder="0"
+allowFullScreen
+allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+loading="lazy"
+title="GIMKHANA Party Anthems on Spotify"
+/>
+);
+}

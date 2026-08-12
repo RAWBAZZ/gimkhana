d4a6aa3d703367437b11c01f6d4f0b17a7751c8a
RAWBAZZ <therealrawbazz@gmail.com>
2026-08-12 17:26:31 +0530

    Update SpotifyPlayer.tsx

--- components/SpotifyPlayer.tsx
+++ components/SpotifyPlayer.tsx
@@ -2,14 +2,14 @@ export default function SpotifyPlayer() {
 return (
 <iframe
 style={{ borderRadius: 16 }}
-src="https://open.spotify.com/embed/playlist/75zwc1fWU4EKVU2jSzkSAq?utm_source=generator&theme=0"
+src="https://open.spotify.com/embed/playlist/0fICF5BxpOk8oLbXoRvVE0?utm_source=generator&theme=0"
 width="100%"
 height="380"
 frameBorder="0"
 allowFullScreen
 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
 loading="lazy"
-title="GIMKHANA Party Anthems on Spotify"
+title="Gymkhana Hindi Hits on Spotify"
 />
 );
 }
